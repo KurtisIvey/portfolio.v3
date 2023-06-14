cb0027e37207f2fab59d714a52e59a75384a46a3
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-06-14 14:26:43 -0500

    finish base tabletplus project card styling

--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState, useCallback, useEffect } from "react";
 import "./Projects.css";
 import useWindowWidth from "../../hooks/useWindowWidth";
 import testImg from "../../assets/previews/space-Tourism.gif";
@@ -10,17 +10,37 @@ const Projects: React.FC = () => {
   // custom hook to detect window size so I can conditionally render specific project card
   const windowWidth = useWindowWidth();
   // lim set to 770px wide to switch view
+  const imgRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const imgContainer = imgRef.current;
+
+    const handleMouseEnter = () => {
+      if (imgContainer) {
+        imgContainer.style.zIndex = "3";
+      }
+    };
+
+    const handleMouseLeave = () => {
+      if (imgContainer) {
+        imgContainer.style.zIndex = "initial";
+      }
+    };
+
+    if (imgContainer) {
+      imgContainer.addEventListener("mouseenter", handleMouseEnter);
+      imgContainer.addEventListener("mouseleave", handleMouseLeave);
+    }
+
+    return () => {
+      if (imgContainer) {
+        imgContainer.removeEventListener("mouseenter", handleMouseEnter);
+        imgContainer.removeEventListener("mouseleave", handleMouseLeave);
+      }
+    };
+  }, []);
 
   const tester = [
-    {
-      name: "Space Tourism",
-      img: testImg,
-      repo: "https://github.com/KurtisIvey/space-tourism-nextjs",
-      website: "https://space-tourism-nextjs-uu85.vercel.app/",
-      techStack: "TypeScript, Next.js, React, Sass",
-      description:
-        "Space Tourism is an interactive website that allows one to explore the different possible destinations should they decide to embark on an out of this world vacation. User's are able to explore the different destinations, the crew members, and the associated technologies that will be used in their space journey.",
-    },
     {
       name: "Space Tourism",
       img: testImg,
@@ -38,11 +58,14 @@ const Projects: React.FC = () => {
       </h1>
       {tester.map((test) => (
         <div className="tabletPlusProjectCard">
-          <div className="imgWrapper">
+          <div className="imgWrapper" ref={imgRef}>
             <img src={test.img} alt="Kurtis Ivey's profile photo" />
             <div className="color-overlay"></div>
           </div>
-          <div className="techStack">{test.techStack}</div>
+          <div className="techStack">
+            <span>Tech Used: </span>
+            {test.techStack}
+          </div>
           {/* positioned absolutely */}
           <h2>
             <em>{test.name}</em>
@@ -52,11 +75,11 @@ const Projects: React.FC = () => {
             <p>{test.description}</p>
             <div className="links">
               <a href={test.repo} aria-label="github repo link">
-                <span>Github Repository</span>
-                <BsGithub size={25} />
-              </a>{" "}
+                <div>Github Repository</div>
+                <BsGithub size={20} />
+              </a>
               <a href={test.website} aria-label="website link">
-                Website <BsGlobe size={25} />
+                Website <BsGlobe size={20} />
               </a>
             </div>
           </div>
