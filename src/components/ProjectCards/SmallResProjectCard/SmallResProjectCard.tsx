221c9d174267d51a856ac46ccfa8f72378eb71cb
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-06-15 14:57:16 -0500

    destructure props

--- src/components/ProjectCards/SmallResProjectCard/SmallResProjectCard.tsx
+++ src/components/ProjectCards/SmallResProjectCard/SmallResProjectCard.tsx
@@ -2,25 +2,32 @@ import "./SmallResProjectCard.css";
 import { BsGithub, BsGlobe } from "react-icons/bs";
 import { ProjectInterface } from "../project.types";
 
-const SmallResProjectCard = (props: ProjectInterface) => {
+const SmallResProjectCard = ({
+  img,
+  name,
+  techStack,
+  description,
+  repo,
+  website,
+}: ProjectInterface) => {
   return (
     <article className="mobileProjectCard">
       <div className="top">
-        <h2>{props.name}</h2>
+        <h2>{name}</h2>
         <div className="links">
-          <a href={props.repo} aria-label="github repo link">
+          <a href={repo} aria-label="github repo link">
             <BsGithub size={25} />
           </a>{" "}
-          <a href={props.website} aria-label="website link">
+          <a href={website} aria-label="website link">
             <BsGlobe size={25} />
           </a>
         </div>
       </div>
-      <img src={props.img} alt={props.name} />
+      <img src={img} alt={name} />
       <p className="techStack">
-        <span>Tech Used:</span> {props.techStack}
+        <span>Tech Used:</span> {techStack}
       </p>
-      <p className="description">{props.description}</p>
+      <p className="description">{description}</p>
     </article>
   );
 };
