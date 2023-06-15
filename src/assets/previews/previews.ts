3007f97db09cc8a2f96eee2d16207921b48470ad
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-06-15 13:48:31 -0500

    upload optimized gifs and create previews.ts to export projects array for use in projects

---
+++ src/assets/previews/previews.ts
@@ -0,0 +1,34 @@
+import jiuJitsuConnectGif from "./jiuJitsuConnect.gif";
+import bobaDepotGif from "./bobaDepot.gif";
+import spaceTourismGif from "./spaceTourism.gif";
+
+export const projects = [
+  {
+    name: "Jiu Jitsu Connect",
+    img: jiuJitsuConnectGif,
+    repo: "https://github.com/KurtisIvey/space-tourism-nextjs",
+    website: "https://jiujitsu-connect.herokuapp.com/",
+    techStack:
+      "React, Redux, TypeScript, JavaScript, Tailwind CSS, Node.js, Express, MongoDB, PostgreSQL, AWS S3, Heroku",
+    description:
+      "Social Media platform that allows practicioners to communicate and connect with each other. Members are able to register, login, upload profile photos, post, like, make friends, and more!",
+  },
+  {
+    name: "Space Tourism!",
+    img: spaceTourismGif,
+    repo: "https://github.com/KurtisIvey/space-tourism-nextjs",
+    website: "https://space-tourism-nextjs-uu85.vercel.app/",
+    techStack: "TypeScript, Next.js, React, CSS, Sass",
+    description:
+      "Space Tourism is an interactive website that allows one to explore the different possible destinations should they decide to embark on an out of this world vacation. User's are able to explore the different destinations, the crew members, and the associated technologies that will be used in their space journey.",
+  },
+  {
+    name: "Boba Depot",
+    img: bobaDepotGif,
+    repo: "https://github.com/KurtisIvey/boba_project",
+    website: "https://boba-project-ki.herokuapp.com/",
+    techStack: "Node.js, Express, EJS, MongoDB, CSS, AWS S3",
+    description:
+      "A server sided boba drink aggregation platform that allows one to store different kinds of boba drinks. Drinks are categorized via temperature. New boba drinks and images of said drinks can be added to the platform via AWS S3 and MongoDB.",
+  },
+];
--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Projects.css";
 import useWindowWidth from "../../hooks/useWindowWidth";
 import testImg from "../../assets/previews/space-Tourism.gif";
-
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { projects } from "../../assets/previews/previews";
 import SmallResProjectCard from "../../components/ProjectCards/SmallResProjectCard/SmallResProjectCard";
 import LargeResProjectCard from "../../components/ProjectCards/LargeResProjectCard/LargeResProjectCard";
 
@@ -10,8 +12,19 @@ const Projects: React.FC = () => {
   // custom hook to detect window size so I can conditionally render specific project card
   const windowWidth = useWindowWidth();
   // lim set to 770px wide to switch view
+  const controls = useAnimation();
+  const [ref, inView] = useInView({
+    triggerOnce: true, // Trigger the animation only once when the div enters the viewport
+  });
 
-  const projects = [
+  useEffect(() => {
+    if (inView) {
+      // motion features definied here to be used on the section element
+      controls.start({ opacity: 1, transition: { duration: 1.3 } });
+    }
+  }, [controls, inView]);
+
+  /* const projects = [
     {
       name: "Space Tourism",
       img: testImg,
@@ -21,9 +34,15 @@ const Projects: React.FC = () => {
       description:
         "Space Tourism is an interactive website that allows one to explore the different possible destinations should they decide to embark on an out of this world vacation. User's are able to explore the different destinations, the crew members, and the associated technologies that will be used in their space journey.",
     },
-  ];
+  ]; */
   return (
-    <section className="projectsSection">
+    <motion.section
+      ref={ref}
+      className="projectsSection"
+      id="projects"
+      initial={{ opacity: 0 }}
+      animate={controls}
+    >
       <h1>
         <span>03.</span> Projects
       </h1>
@@ -48,7 +67,7 @@ const Projects: React.FC = () => {
           />
         )
       )}
-    </section>
+    </motion.section>
   );
 };
 
