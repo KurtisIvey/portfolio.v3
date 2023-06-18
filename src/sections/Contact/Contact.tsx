b88b5b99f0626d51fc8d085be8b8042015451ccc
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-06-18 15:25:08 -0500

    add css import and h1 header for contact

--- src/sections/Contact/Contact.tsx
+++ src/sections/Contact/Contact.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-
+import "./Contact.css";
 type Props = {};
 
 const Contact = (props: Props) => {
-  return <div>Contact</div>;
+  return (
+    <section>
+      <h1>
+        <span>04. </span>Contact
+      </h1>
+    </section>
+  );
 };
 
 export default Contact;
