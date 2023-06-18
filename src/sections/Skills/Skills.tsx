178bdbf62bb305cff3a2e45c0e2f93155445c30e
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-06-18 14:55:50 -0500

    adjust gap

--- src/sections/Skills/Skills.tsx
+++ src/sections/Skills/Skills.tsx
@@ -4,7 +4,7 @@ import skills from "../../assets/skillLogos/skillLogos";
 import { v4 as uuidv4 } from "uuid";
 import SkillLogo from "../../components/SkillLogo/SkillLogo";
 
-const Skills = (props: Props) => {
+const Skills = () => {
   return (
     <section className="skillsSection subSection">
       <h1 className="sectionH1">
