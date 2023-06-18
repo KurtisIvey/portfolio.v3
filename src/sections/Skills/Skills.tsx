b967dc3a363527731ffad34c8e4afd11390a5cb6
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-06-18 13:39:21 -0500

    update default svg height & width to 64

--- src/sections/Skills/Skills.tsx
+++ src/sections/Skills/Skills.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import "./Skills.css";
-import skillLogos from "../../assets/skillLogos/skillLogos";
+import skills from "../../assets/skillLogos/skillLogos";
+import { v4 as uuidv4 } from "uuid";
+
 const Skills = (props: Props) => {
   return (
     <section className="skillsSection">
       <h1>
         <span>02. </span>Skills
       </h1>
-      <div className="logos">{}</div>
+      <div className="logosContainer">
+        {skills.map((skill) => (
+          <div key={uuidv4()} className="logo">
+            <img src={skill.url} alt={skill.name} />
+            <p>{skill.name}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
