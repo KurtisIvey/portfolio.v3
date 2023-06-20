b3b3cec89cdd200eca5ffd71ad132ec4f5f013f3
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-06-20 16:53:04 -0500

    update projectInterface

--- src/components/ProjectCards/project.types.ts
+++ src/components/ProjectCards/project.types.ts
@@ -2,7 +2,8 @@ export interface ProjectInterface {
   name: string;
   repo: string;
   website: string;
-  img: string;
+  img?: string;
+  mp4?: string;
   techStack: string;
   description: string;
 }
