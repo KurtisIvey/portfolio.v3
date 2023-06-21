d6cddd00add1adf11372f2c8d43f4bc01e187726
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-06-21 18:52:39 -0500

    add custom web solution badge section

--- src/sections/Solutions/Solutions.tsx
+++ src/sections/Solutions/Solutions.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import "./Solutions.css";
+import { BsGearWideConnected } from "react-icons/bs";
 
 const Solutions = () => {
   return (
     <div className="solutionsContainer">
       <h1 className="sectionH1">Services I Offer</h1>
+      <div className="shieldBox">
+        <div className="hexagon-shield">
+          <BsGearWideConnected size={75} className="rotate" />
+        </div>
+        <h2>Custom Web Solutions</h2>
+        <p>
+          I provide custom web solutions using modern technologies like React,
+          TypeScript, JavaScript, HTML, CSS, Node.js, Express, and MongoDB to
+          create tailored, scalable, and efficient web applications for your
+          specific needs.
+        </p>
+      </div>
     </div>
   );
 };
