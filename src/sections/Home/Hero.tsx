91c3147e4191d3e9c04f3ec8c4f90b2e47806771
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-06-21 20:53:26 -0500

    fix hero section

--- src/sections/Home/Hero.tsx
+++ src/sections/Home/Hero.tsx
@@ -23,19 +23,22 @@ const Hero = () => {
       whileInView={{ opacity: 1 }}
       viewport={{ once: true }}
     >
-      <h1 className="hiMyNameIs">Hi, my name is</h1>
-      <h1 className="kurtisIvey">Kurtis Ivey.</h1>
-      <h1 className="typeWriterText">
-        <span>{text}</span>
-        <Cursor cursorColor="#ffffff" />
-      </h1>
-      <div className="bg">
-        <p>
-          I'm a developer specializing in building highly interactive, robust,
-          scalable, and maintainable web applications on both the front and back
-          end using JavaScript frameworks. I'm currently looking for work and
-          freelancing opportunities.
-        </p>
+      {" "}
+      <div className="heroContainer">
+        <h1 className="hiMyNameIs">Hi, my name is</h1>
+        <h1 className="kurtisIvey">Kurtis Ivey.</h1>
+        <h1 className="typeWriterText">
+          <span>{text}</span>
+          <Cursor cursorColor="#ffffff" />
+        </h1>
+        <div className="bg">
+          <p>
+            I'm a developer specializing in building highly interactive, robust,
+            scalable, and maintainable web applications on both the front and
+            back end using JavaScript frameworks. I'm currently looking for work
+            and freelancing opportunities.
+          </p>
+        </div>
       </div>
     </motion.section>
   );
