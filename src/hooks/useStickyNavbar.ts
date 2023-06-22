0d2673c3bced0f2438f3b30a49ca62d1b6c1766d
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-06-21 20:06:40 -0500

    add hook

---
+++ src/hooks/useStickyNavbar.ts
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+function useStickyNavbar(): boolean {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return isSticky;
+}
+
+export default useStickyNavbar;
