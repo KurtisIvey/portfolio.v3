bc0a93ab185fa58ed6068d1f21488bf3a3ec2760
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-06-23 14:46:01 -0500

    create smallNavbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,23 +5,13 @@ import { motion } from "framer-motion";
 
 import "./Navbar.css";
 import useWindowWidth from "../../hooks/useWindowWidth";
+import SmallNavbar from "./SmallNavbar/SmallNavbar";
 
 const Navbar = () => {
-  const [sideBarOpen, setSideBarOpen] = useState(false);
   // prevents unnecessary rendering of both sidebars at once by detecting window width
   // windowWidth becomes the measurement for the ternary operator
   const windowWidth = useWindowWidth();
-  const toggleSideBar = useCallback(() => {
-    setSideBarOpen((prevOpen) => !prevOpen);
-  }, []);
-
-  useEffect(() => {
-    if (sideBarOpen) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "auto";
-    }
-  }, [sideBarOpen]);
+  const [sideBarOpen, setSideBarOpen] = useState(false);
 
   const handleScrollToSection = (sectionId: string) => {
     const section = document.getElementById(sectionId);
@@ -47,69 +37,11 @@ const Navbar = () => {
         Kurtis <span>Ivey</span>
       </h1>
       {windowWidth < 770 ? (
-        <>
-          {sideBarOpen && <div className="blur-filter" />}{" "}
-          <nav>
-            <button
-              className={`hamburger-container ${sideBarOpen ? "open" : ""}`}
-              onClick={toggleSideBar}
-              aria-label={sideBarOpen ? "Close Menu" : "Open Menu"}
-              aria-expanded={sideBarOpen}
-            >
-              {sideBarOpen ? (
-                <AiOutlineClose size={45} className="hamburger" />
-              ) : (
-                <BiMenuAltRight size={45} className="hamburger" />
-              )}
-            </button>
-            <div
-              id="side-bar"
-              className={sideBarOpen ? "side-bar-open" : ""}
-              role="menu"
-              aria-label="Main Menu"
-            >
-              <ul role="menu">
-                <hr />
-                <li role="none">
-                  <button
-                    onClick={() => handleScrollToSection("about")}
-                    aria-label="About Section"
-                    role="menuitem"
-                  >
-                    <span>01.</span> About
-                  </button>
-                </li>
-                <li role="none">
-                  <button
-                    onClick={() => handleScrollToSection("skills")}
-                    aria-label="Skills Section"
-                    role="menuitem"
-                  >
-                    <span>02.</span> Skills
-                  </button>
-                </li>
-                <li role="none">
-                  <button
-                    onClick={() => handleScrollToSection("projects")}
-                    aria-label="Projects Section"
-                    role="menuitem"
-                  >
-                    <span>03.</span> Projects
-                  </button>
-                </li>
-                <li role="none">
-                  <button
-                    onClick={() => handleScrollToSection("contact")}
-                    aria-label="Contact Section"
-                    role="menuitem"
-                  >
-                    <span>04.</span> Contact
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </>
+        <SmallNavbar
+          sideBarOpen={sideBarOpen}
+          setSideBarOpen={setSideBarOpen}
+          handleScrollToSection={handleScrollToSection}
+        />
       ) : (
         <nav className="nav-larger-res-nav-links">
           <ul className="larger-nav-links-container" role="menu">
---
+++ src/components/Navbar/SmallNavbar/SmallNavbar.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import "./SmallNavbar.css";
+import { useState, useEffect, useCallback } from "react";
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
+type Props = {
+  handleScrollToSection: (sectionId: string) => void;
+  sideBarOpen: boolean;
+  setSideBarOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const SmallNavbar = ({
+  handleScrollToSection,
+  sideBarOpen,
+  setSideBarOpen,
+}: Props) => {
+  const toggleSideBar = useCallback(() => {
+    setSideBarOpen((prevOpen) => !prevOpen);
+  }, [setSideBarOpen]);
+
+  return (
+    <>
+      {sideBarOpen && <div className="blur-filter" />}{" "}
+      <nav>
+        <button
+          className={`hamburger-container ${sideBarOpen ? "open" : ""}`}
+          onClick={toggleSideBar}
+          aria-label={sideBarOpen ? "Close Menu" : "Open Menu"}
+          aria-expanded={sideBarOpen}
+        >
+          {sideBarOpen ? (
+            <AiOutlineClose size={45} className="hamburger" />
+          ) : (
+            <BiMenuAltRight size={45} className="hamburger" />
+          )}
+        </button>
+        <div
+          id="side-bar"
+          className={sideBarOpen ? "side-bar-open" : ""}
+          role="menu"
+          aria-label="Main Menu"
+        >
+          <ul role="menu">
+            <hr />
+            <li role="none">
+              <button
+                onClick={() => handleScrollToSection("about")}
+                aria-label="About Section"
+                role="menuitem"
+              >
+                <span>01.</span> About
+              </button>
+            </li>
+            <li role="none">
+              <button
+                onClick={() => handleScrollToSection("skills")}
+                aria-label="Skills Section"
+                role="menuitem"
+              >
+                <span>02.</span> Skills
+              </button>
+            </li>
+            <li role="none">
+              <button
+                onClick={() => handleScrollToSection("projects")}
+                aria-label="Projects Section"
+                role="menuitem"
+              >
+                <span>03.</span> Projects
+              </button>
+            </li>
+            <li role="none">
+              <button
+                onClick={() => handleScrollToSection("contact")}
+                aria-label="Contact Section"
+                role="menuitem"
+              >
+                <span>04.</span> Contact
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default SmallNavbar;
