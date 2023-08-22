a0c19ee8a276489606b79432fb5ab2d57c61bc3e
KurtisIvey <kurtiveycodes@gmail.com>
2023-08-22 06:27:16 -0500

    finish back to top

--- src/components/BackToTop/BackToTop.tsx
+++ src/components/BackToTop/BackToTop.tsx
@@ -1,33 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./BackToTop.css";
+import { BsArrowUpCircleFill } from "react-icons/bs";
 
 const BackToTop: React.FC = () => {
-  const [isVisible, setIsVisible] = useState(false);
-
-  const handleScroll = () => {
-    const scrollTop = window.pageYOffset;
-    const isVisible = scrollTop > 0;
-    setIsVisible(isVisible);
-  };
-
   const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    const section = document.getElementById("top");
+    section && section.scrollIntoView({ behavior: "smooth" });
   };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   return (
     <div className="back-to-top">
-      {isVisible ? (
-        <button onClick={scrollToTop} className="back-to-top-button">
-          Back to top
-        </button>
-      ) : null}
+      <button onClick={scrollToTop} className="back-to-top-button">
+        <BsArrowUpCircleFill size={50} />
+      </button>
     </div>
   );
 };
